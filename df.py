95e27ac38c83ad1f5680f2b814d1f5110682378b
Kyaw Thiha <kyawthiha89@live.com>
2018-04-23 11:08:27 -0400

    domain list add

--- df.py
+++ df.py
@@ -1,35 +1,37 @@
-#!/usr/bin/python
+#!/usr/bin/python 
 
 import sys
 import requests
 import os
 import argparse
+import optparse
 
+def listvendor_callback(option, opt_str, value, parser):
+	print "support vendor:\n\x1b[6;30;42m akama \n aliyun \n azure \n beluga \n cdn77 \n chinacache \n cloudfront \n fastly \n maxcdn \n netlify \n stackpath \x1b[0m"
+	
 
-print('''### domain fronting finder
-### domain lists can be found @ https://github.com/vysec/DomainFrontingLists
+print('''### domain fronting finder 
+### domain lists can be found @ https://github.com/vysec/DomainFrontingListsa
 ### script idea derive from https://github.com/yeyintminthuhtut/Lazy-RedTeamer-Scripts\n''')
 
 if len(sys.argv) == 1:
-        print "Usage : python df.py paramter \n python df.py -h"
-        sys.exit()
+	print "Usage : python df.py paramter \n python df.py -h \n python fd.py -H host -v cloudfront"
+	sys.exit()
 
-parser = argparse.ArgumentParser(description="  Domain Finding Script \n",
-                                prog="s3")
+parser = optparse.OptionParser(description="  Domain Finding Script \n",
+				prog="df.py")
 
+parser.add_option('-H','--domain',dest='host',help='your host for fronting') 
+parser.add_option('-v','--vendor',dest='vendor',help='CDN Vendor')
+parser.add_option('-l','--listvendor',dest='vendorlist',help='vendor list', action='callback', callback=listvendor_callback)
 
-parser.add_argument('-f','--file',dest='file',help='file of domain')
-parser.add_argument('-H','--domain',dest='host',help='your host for fronting')
-parser.add_argument('-v','--vendor',dest='vendor',help='domain vendor:cloudfront,akamai,cloudflare,etc..')
+options, args = parser.parse_args()
 
-args = parser.parse_args()
-
-if args.vendor == 0:
-        print "Vendor parameter is require"
-
-
-if (args.vendor == "aws") or (args.vendor == "akamai") or (args.vendor == "azure") or (args.vendor == "cdn77"):
-        with open(args.file, 'r') as f:
+if options.vendor == 0:
+	print "Vendor parameter is requier"
+   
+if (options.vendor == "cloudfront") or (options.vendor == "akamai") or (options.vendor == "azure") or (options.vendor == "cdn77"):
+	with open("domainlists/"+args.vendor+".txt", 'r') as f:
                 for line in f:
                         host = line.strip()
                         hosts = "http://"+host+"/t33t.txt"
@@ -43,5 +45,6 @@ if (args.vendor == "aws") or (args.vendor == "akamai") or (args.vendor == "azure
                         else :
                                 print "[~] " +host+ ": \x1b[3;30;47m"+ "not Frontable" +"\x1b[0m "
 
-else :
-        print "[~] vendor name error \n[~] support vendor akamai, aliyun, azure,beluga, cdn77, chinacache, cloudfront, fastly, maxcdn, netlify, stackpath"
+#else:
+#	print "support vendor:\n\x1b[6;30;42m akama \n aliyun \n azure \n beluga \n cdn77 \n chinacache \n cloudfront \n fastly \n maxcdn \n netlify \n stackpath \x1b[0m"
+	
